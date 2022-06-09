c5e1cd32d2836984c131304ac427aa1c0023dcdf
Thomas <thomas@amyuni.com>
2022-06-09 16:08:36 +0100

    It builds!

---
+++ premake5.lua
@@ -0,0 +1,92 @@
+-- Assimp solution
+workspace "Assimp"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Assimp project
+project "Assimp"
+	--location "Assimp"
+
+	kind "StaticLib"
+	staticruntime "On"
+
+	language "C++"
+	cppdialect "C++20"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.c",
+		"**.hpp",
+		"**.cpp"
+	}
+
+	includedirs
+	{
+		"./",
+		"include",
+		"code",
+		"contrib",
+		"contrib/pugixml/src",
+		"contrib/zlib",
+		"contrib/unzip",
+		"contrib/rapidjson/include",
+		"contrib/openddlparser/include"
+		--"contrib/zip",
+		--"contrib/zip"
+	}
+
+	defines 
+	{ 
+		"_CRT_SECURE_NO_WARNINGS",
+
+		"ASSIMP_WARNINGS_AS_ERRORS",
+		"ASSIMP_IGNORE_GIT_HASH",
+
+		"OPENDDL_STATIC_LIBARY",
+
+		"ASSIMP_BUILD_NO_EXPORT",
+
+		"ASSIMP_BUILD_NO_3MF_IMPORTER",
+		"ASSIMP_BUILD_NO_AC_IMPORTER",
+		"ASSIMP_BUILD_NO_AMF_IMPORTER",
+		"ASSIMP_BUILD_NO_ASE_IMPORTER",
+		"ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+		"ASSIMP_BUILD_NO_C4D_IMPORTER",
+		"ASSIMP_BUILD_NO_COB_IMPORTER",
+		"ASSIMP_BUILD_NO_CSM_IMPORTER",
+		"ASSIMP_BUILD_NO_DXF_IMPORTER",
+		"ASSIMP_BUILD_NO_IFC_IMPORTER",
+		"ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+		"ASSIMP_BUILD_NO_MD2_IMPORTER",
+		"ASSIMP_BUILD_NO_MD3_IMPORTER",
+		"ASSIMP_BUILD_NO_MD4_IMPORTER",
+		"ASSIMP_BUILD_NO_MD5_IMPORTER",
+		"ASSIMP_BUILD_NO_MDC_IMPORTER",
+		"ASSIMP_BUILD_NO_MDL_IMPORTER",
+		"ASSIMP_BUILD_NO_MMD_IMPORTER",
+		"ASSIMP_BUILD_NO_M3D_IMPORTER"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "On"
